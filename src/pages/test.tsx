14bd641a1071aadb189f2e2404ed0f4f1182da3f
小津 <sunxiaojin@blockvision.org>
2022-10-28 18:30:21 +0800

    feat: localize modes

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,15 +1,18 @@
+import ExampleScene from "@/components/test/ExampleScene";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 export default function Test() {
   return (
-    <div className="h-[100vh] w-full bg-slate-50">
-      <Canvas>
-        <ambientLight />
-        <directionalLight position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry intensity={0.1} />
-          <meshStandardMaterial color="#aac" />
-        </mesh>
+    <div
+      className="h-[100vh] w-full bg-slate-50"
+      style={{
+        background:
+          "linear-gradient(204.64deg, #4F46E5 15.72%, #A855F7 84.28%)",
+      }}
+    >
+      <Canvas frameloop="demand" shadows={true}>
+        <ExampleScene />
       </Canvas>
     </div>
   );

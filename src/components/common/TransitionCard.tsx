881ae3e04c57669e283318a90b4d1cd0f66407fa
suni <yysuni1001@qq.com>
2022-11-01 20:44:28 +0800

    style: card transition

--- src/components/common/TransitionCard.tsx
+++ src/components/common/TransitionCard.tsx
@@ -9,7 +9,7 @@ export default function TransitionCard(props: React.PropsWithChildren & { show: 
 			enter='transition-all duration-600 ease-in'
 			enterFrom='opacity-0 translate-x-12'
 			enterTo='opacity-100'
-			leave='transition-all duration-300 ease-out'
+			leave='transition-all duration-1000 ease-out'
 			leaveFrom='opacity-100'
 			leaveTo='opacity-0 -translate-x-12'>
 			{props.children}
--- src/components/home/HeroScene.tsx
+++ src/components/home/HeroScene.tsx
@@ -1,34 +1,31 @@
-import Spline from "@splinetool/react-spline";
-import Logo from "@/svgs/logo.svg";
-import Web3 from "@/svgs/Web3.svg";
-import Button from "@/components/common/Button";
+import Spline from '@splinetool/react-spline'
+import Logo from '@/svgs/logo.svg'
+import Web3 from '@/svgs/Web3.svg'
+import Button from '@/components/common/Button'
 
 export default function HeroScene() {
-  return (
-    <section className="h-[900px] bg-slate-100 overflow-hidden relative">
-      <div className="absolute w-[2500px] h-[900px] translate-x-[-800px] left-[50%]">
-        {/* <Spline scene="https://prod.spline.design/MRt17A31cw5xjn5J/scene.splinecode" /> */}
-      </div>
+	return (
+		<section className='h-[900px] bg-slate-100 overflow-hidden relative'>
+			<div className='absolute w-[2500px] h-[900px] translate-x-[-800px] left-[50%]'>
+				{/* <Spline scene="https://prod.spline.design/MRt17A31cw5xjn5J/scene.splinecode" /> */}
+			</div>
 
-      <div className="w-[1440px] mx-auto h-full overflow-auto">
-        <div className="ml-[80px] mt-[164px] w-[655px] relative">
-          <Logo />
-          <h1 className="font-unbounded font-[900] text-[60px] leading-[64px] mt-[17px]">
-            <span>Customize your</span>
-            <br />
-            <Web3 />
-            <span>wonderland and lifestyle</span>
-          </h1>
-          <p className="font-medium mt-4 text-lg leading-6 opacity-60">
-            Room Service is the protocol that powers Web3 room facilities,
-            enabling users to build, launch and host their own blockchain-based
-            spaces without any coding experience.
-          </p>
-          <Button className="mt-8 font-bold font-unbounded">
-            Explore Demo
-          </Button>
-        </div>
-      </div>
-    </section>
-  );
+			<div className='w-[1440px] mx-auto h-full overflow-auto'>
+				<div className='ml-[80px] mt-[164px] w-[655px] relative'>
+					<Logo />
+					<h1 className='font-unbounded font-[900] text-[60px] leading-[64px] mt-[17px]'>
+						<span>Customize your</span>
+						<br />
+						<Web3 />
+						<span>wonderland and lifestyle</span>
+					</h1>
+					<p className='font-medium mt-4 text-lg leading-6 opacity-60'>
+						Room Service is the protocol that powers Web3 room facilities, enabling users to build, launch and host
+						their own blockchain-based spaces without any coding experience.
+					</p>
+					<Button className='mt-8 font-bold font-unbounded'>Explore Demo</Button>
+				</div>
+			</div>
+		</section>
+	)
 }

a0861050ccdb9a9dd4d0d7f221796b745bd28af7
suni <YYsuni1001@gmail.com>
2022-12-06 15:57:22 +0800

    feat(about): dynamic banner

--- src/components/about/Banner.tsx
+++ src/components/about/Banner.tsx
@@ -1,20 +1,43 @@
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const SINGLE_WIDTH = 1010
+
 export default function Banner() {
+	const { x } = useSpring({
+		from: { x: -SINGLE_WIDTH * 5 },
+		to: { x: 0 },
+		loop: true,
+		config: {
+			duration: 80000
+		}
+	})
+
 	return (
-		<svg className="mx-auto" width="1440" height="132" viewBox="0 0 1440 132" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<svg>
-		<text x="-130" y="105" className="text-[100px] font-mona font-[900]" fill="#1c1c1c">ROOM SERVICE</text>
-		<path d="M823.513 17.5C841.736 17.5 856.513 32.3128 856.513 50.5909V115.5H790.513V50.5909C790.513 32.3128 805.29 17.5 823.513 17.5Z" stroke="#1C1C1C" stroke-width="2"/>
-		<path d="M823.512 57C834.224 57 842.912 65.7076 842.912 76.4545V115H804.112V76.4545C804.112 65.7076 812.8 57 823.512 57Z" stroke="#1C1C1C" stroke-width="2"/>
-		<path d="M823.512 87C828.591 87 832.712 91.1287 832.712 96.2273V115H814.312V96.2273C814.312 91.1287 818.434 87 823.512 87Z" stroke="#1C1C1C" stroke-width="2"/>
-		</svg>
-		<svg x='882'>
-		<text x="0" y="105" className="text-[100px] font-mona font-[900]" fill="#1c1c1c">ROOM SERVICE</text>
-		<path d="M1776.513 17.5C841.736 17.5 856.513 32.3128 856.513 50.5909V115.5H790.513V50.5909C790.513 32.3128 805.29 17.5 823.513 17.5Z" stroke="#1C1C1C" stroke-width="2"/>
-		<path d="M1776.512 57C834.224 57 842.912 65.7076 842.912 76.4545V115H804.112V76.4545C804.112 65.7076 812.8 57 823.512 57Z" stroke="#1C1C1C" stroke-width="2"/>
-		<path d="M1776.512 87C828.591 87 832.712 91.1287 832.712 96.2273V115H814.312V96.2273C814.312 91.1287 818.434 87 823.512 87Z" stroke="#1C1C1C" stroke-width="2"/>
-		</svg>
-		<path d="M0 1H1440V-1H0V1ZM1440 131H0V133H1440V131Z" fill="#1C1C1C" mask="url(#path-1-inside-1_1301_38150)"/>
+		<svg
+			className='mx-auto'
+			width='1440'
+			height='132'
+			viewBox='0 0 1440 132'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<animated.svg x={x} width={SINGLE_WIDTH * 10}>
+				{Array(10)
+					.fill(1)
+					.map((item, i) => (
+						<React.Fragment key={i}>
+							<text x={i * SINGLE_WIDTH} y='105' className='font-mona text-[100px] font-[900]' fill='#1c1c1c'>
+								ROOM SERVICE
+							</text>
+							<svg stroke='#1C1C1C' strokeWidth='2' x={130 + SINGLE_WIDTH * i}>
+								<path d='M823.513 17.5C841.736 17.5 856.513 32.3128 856.513 50.5909V115.5H790.513V50.5909C790.513 32.3128 805.29 17.5 823.513 17.5Z' />
+								<path d='M823.512 57C834.224 57 842.912 65.7076 842.912 76.4545V115H804.112V76.4545C804.112 65.7076 812.8 57 823.512 57Z' />
+								<path d='M823.512 87C828.591 87 832.712 91.1287 832.712 96.2273V115H814.312V96.2273C814.312 91.1287 818.434 87 823.512 87Z' />
+							</svg>
+						</React.Fragment>
+					))}
+			</animated.svg>
+			<path d='M0 1H1440V-1H0V1ZM1440 131H0V133H1440V131Z' fill='#1C1C1C' mask='url(#path-1-inside-1_1301_38150)' />
 		</svg>
 	)
-	
 }
--- src/components/about/BannerThin.tsx
+++ src/components/about/BannerThin.tsx
@@ -1,5 +1,35 @@
-import BannerSvg from '@/svgs/banner-thin.svg'
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const SINGLE_WIDTH = 240
 
 export default function BannerThin() {
-	return <BannerSvg className='mx-auto' />
+	const { x } = useSpring({
+		from: { x: -SINGLE_WIDTH * 10 },
+		to: { x: 0 },
+		loop: true,
+		config: {
+			duration: 60000
+		}
+	})
+
+	return (
+		<svg
+			className='mx-auto bg-dark'
+			width='1440'
+			height='40'
+			viewBox='0 0 1440 40'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<animated.svg x={x} width={SINGLE_WIDTH * 20}>
+				{Array(20)
+					.fill(0)
+					.map((item, i) => (
+						<text key={i} x={i * SINGLE_WIDTH} y='26' fill='white' className='font-mona font-[600]'>
+							ROOM SERVICE
+						</text>
+					))}
+			</animated.svg>
+		</svg>
+	)
 }

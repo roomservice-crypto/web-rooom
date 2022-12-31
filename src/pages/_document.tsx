3d4b4db78010c0adc4849dcaf7d181dbdfcf6043
Suni <yysuni1001@gmail.com>
2022-12-31 15:59:08 +0800

    Update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,15 @@ class MyDocument extends Document {
 			<Html lang='en'>
 				<Head>
 					<meta lang='en' />
-					<link rel='icon' href='/logo.svg' />
+
+					<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+					<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+					<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+					<link rel='manifest' href='/site.webmanifest' />
+					<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+					<meta name='msapplication-TileColor' content='#ecff16' />
+					<meta name='theme-color' content='#ffffff' />
+
 					<meta name='description' content={DESCRIPTION} />
 					<meta property='og:description' content={DESCRIPTION} />
 					<meta name='twitter:description' content={DESCRIPTION} />

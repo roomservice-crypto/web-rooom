04f8988df28b52fd0947da3500b68c488a961b04
roomlukas <lukas@roomservice.gg>
2023-05-07 05:07:43 +0800

    Form init

--- src/pages/rooms/create.tsx
+++ src/pages/rooms/create.tsx
@@ -2,21 +2,31 @@ import Modal from '@/components/Modal'
 import HeaderBar from '@/components/map/HeaderBar'
 import {
 	Box,
+	Button,
+	Card,
 	Dialog,
 	Divider,
+	FormControl,
 	Grid,
+	IconButton,
 	Step,
+	StepConnector,
 	StepIcon,
 	StepIconProps,
 	StepLabel,
 	Stepper,
-	Typography
+	TextField,
+	Typography,
+	stepConnectorClasses,
+	styled
 } from '@mui/material'
-import { ReactElement, useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import MetamaskLogo from '@/assets/img/metamask.svg'
 import WalletConnectLogo from '@/assets/img/walletconnect.svg'
 import CoinbaseLogo from '@/assets/img/coinbasewallet.svg'
-import Roombackground from '@/assets/img/backgroundroom.png'
+import Step1 from '@/svgs/step1.svg'
+import Step2 from '@/svgs/step2gray.svg'
+import Pin from '@/svgs/location.svg'
 
 declare const window: {
 	ethereum?: any
@@ -31,6 +41,13 @@ export default function Create() {
 	const handleClose = () => setOpen(false)
 
 	const [activeStep, setActiveStep] = useState(0)
+	const [completed, setCompleted] = useState(false)
+
+	useEffect(() => {
+		if (activeStep === 2) {
+			setCompleted(true)
+		}
+	}, [activeStep])
 
 	const handleNext = () => {
 		setActiveStep(prevActiveStep => prevActiveStep + 1)
@@ -44,15 +61,54 @@ export default function Create() {
 		setActiveStep(0)
 	}
 
+	const NumberConnector = styled(StepConnector)(({ theme }) => ({
+		[`&.${stepConnectorClasses.vertical}`]: {
+			[`&.${stepConnectorClasses.active}`]: {
+				[`& .${stepConnectorClasses.line}`]: {
+					borderColor: '#0000001A'
+				}
+			},
+			[`&.${stepConnectorClasses.completed}`]: {
+				[`& .${stepConnectorClasses.line}`]: {
+					borderColor: '#FAE76C'
+				}
+			}
+		}
+	}))
+
+	const NumberStepIconRoot = styled('div')<{
+		ownerState: { completed?: boolean; active?: boolean }
+	}>(({ ownerState }) => ({
+		backgroundColor: '#0000001A',
+		zIndex: 1,
+		width: 32,
+		height: 32,
+		display: 'flex',
+		borderRadius: '12px',
+		justifyContent: 'center',
+		alignItems: 'center',
+		...(ownerState.active && {
+			backgroundColor: '#FAE76C',
+			boxShadow: '0 4px 4px 0 rgba(0,0,0,.10)'
+		}),
+		...(ownerState.completed && {
+			backgroundImage: '#FAE76C'
+		})
+	}))
+
 	function NumberStepIcon(props: StepIconProps) {
 		const { active, completed, className } = props
 
-		const icons: { [index: string]: ReactElement } = {
-			1: <MetamaskLogo />,
-			2: <CoinbaseLogo />
+		const icons: { [index: string]: number } = {
+			1: 1,
+			2: 2
 		}
 
-		return icons[String(props.icon)]
+		return (
+			<NumberStepIconRoot ownerState={{ completed, active }} className={className}>
+				{icons[String(props.icon)]}
+			</NumberStepIconRoot>
+		)
 	}
 
 	return (
@@ -66,7 +122,7 @@ export default function Create() {
 			}}>
 			<HeaderBar ready={ready} setRoom={setRoom} />
 
-			<Modal maxWidth='724px' customIsOpen={open} borderRadius='46px' height='400px'>
+			<Modal maxWidth='724px' customIsOpen={open} borderRadius='46px'>
 				<Box
 					display='flex'
 					width='100%'
@@ -77,26 +133,27 @@ export default function Create() {
 						border: '2px solid',
 						borderRadius: '34px'
 					}}>
-					<Grid container>
-						<Grid item xs={2}>
-							<Box
-								sx={{
-									height: '100%',
-									backgroundColor: 'white',
-									borderTopLeftRadius: '34px',
-									borderBottomLeftRadius: '34px',
-									paddingTop: '36px',
-									paddingLeft: '24px'
-								}}>
-								<Stepper activeStep={activeStep} orientation='vertical'>
-									<Step key={1}>
-										<StepLabel StepIconComponent={NumberStepIcon}></StepLabel>
-									</Step>
-									<Step key={2}>
-										<StepLabel StepIconComponent={NumberStepIcon}></StepLabel>
-									</Step>
-								</Stepper>
-								{/* <Box
+					{!completed ? (
+						<Grid container>
+							<Grid item xs={2}>
+								<Box
+									sx={{
+										height: '100%',
+										backgroundColor: 'white',
+										borderTopLeftRadius: '34px',
+										borderBottomLeftRadius: '34px',
+										paddingTop: '36px',
+										paddingLeft: '24px'
+									}}>
+									<Stepper activeStep={activeStep} orientation='vertical' connector={<NumberConnector />}>
+										<Step key={1}>
+											<StepLabel StepIconComponent={NumberStepIcon}></StepLabel>
+										</Step>
+										<Step key={2}>
+											<StepLabel StepIconComponent={NumberStepIcon}></StepLabel>
+										</Step>
+									</Stepper>
+									{/* <Box
 									display='flex'
 									sx={{
 										justifyContent: 'center',
@@ -122,73 +179,151 @@ export default function Create() {
 									}}>
 									2
 								</Box> */}
-							</Box>
+								</Box>
+							</Grid>
+							<Grid item xs={10}>
+								{activeStep == 0 && (
+									<Box paddingTop={8}>
+										<Typography fontSize={24} fontWeight={600} padding={24}>
+											CONNECT TO A WALLET
+										</Typography>
+										<Divider sx={{ margin: '24px', border: '2px dashed #1C1C1C' }} />
+										<Grid container spacing={12} padding={24}>
+											<Grid item xs={4}>
+												<Card
+													onClick={() => {
+														handleNext()
+													}}
+													sx={{
+														display: 'flex',
+														alignItems: 'center',
+														flexDirection: 'column',
+														justifyContent: 'center',
+														height: '180px',
+														width: '180px',
+														borderRadius: '24px',
+														border: '2px solid'
+													}}>
+													<MetamaskLogo />
+													<Typography fontSize={14} fontWeight={500}>
+														MetaMask
+													</Typography>
+												</Card>
+											</Grid>
+											<Grid item xs={4}>
+												<Card
+													onClick={() => {
+														handleBack()
+													}}
+													sx={{
+														display: 'flex',
+														alignItems: 'center',
+														flexDirection: 'column',
+														justifyContent: 'center',
+														height: '180px',
+														width: '180px',
+														borderRadius: '24px',
+														border: '2px solid'
+													}}>
+													<WalletConnectLogo />
+													<Typography fontSize={14} fontWeight={500}>
+														Wallet Connect
+													</Typography>
+												</Card>
+											</Grid>
+											<Grid item xs={4}>
+												<Card
+													sx={{
+														display: 'flex',
+														alignItems: 'center',
+														flexDirection: 'column',
+														justifyContent: 'center',
+														height: '180px',
+														width: '180px',
+														borderRadius: '24px',
+														border: '2px solid'
+													}}>
+													<CoinbaseLogo />
+													<Typography fontSize={14} fontWeight={500}>
+														Coinbase Wallet
+													</Typography>
+												</Card>
+											</Grid>
+										</Grid>
+									</Box>
+								)}
+								{activeStep === 1 && (
+									<Box paddingTop={8} paddingLeft={24} paddingRight={24}>
+										<Typography fontSize={24} fontWeight={600} paddingTop={24}>
+											CREATE ROOM
+										</Typography>
+										<Divider sx={{ margin: '24px 0px 24px 0px', border: '2px dashed #1C1C1C' }} />
+										<FormControl fullWidth>
+											<Typography fontWeight={500} fontSize={16} paddingBottom={8}>
+												Room name
+											</Typography>
+											<TextField
+												variant='outlined'
+												fullWidth
+												placeholder='The name you want to give to your room'></TextField>
+											<Typography fontWeight={500} fontSize={16} paddingBottom={8} marginTop={24}>
+												Select room address
+											</Typography>
+											<TextField
+												InputProps={{
+													endAdornment: (
+														<IconButton onClick={() => 0}>
+															<Pin />
+														</IconButton>
+													)
+												}}
+												fullWidth
+												placeholder='Select a location for your room'></TextField>
+											<Typography fontWeight={500} fontSize={16} paddingBottom={8} marginTop={24}>
+												Description (optional){' '}
+											</Typography>
+											<TextField fullWidth multiline placeholder='Give a description to your room' rows={4}></TextField>
+											<Button
+												onClick={() => {
+													handleNext()
+												}}
+												sx={{ marginTop: '20px', marginBottom: '48px' }}
+												variant='outlined'>
+												Create
+											</Button>
+										</FormControl>
+									</Box>
+								)}
+							</Grid>
 						</Grid>
-						<Grid item xs={10}>
-							<Box paddingTop={8}>
-								<Typography fontSize={24} fontWeight={600} padding={24}>
-									CONNECT TO A WALLET
-								</Typography>
-								<Divider sx={{ margin: '24px', border: '2px dashed #1C1C1C' }} />
-								<Grid container spacing={12} padding={24}>
-									<Grid item xs={4}>
-										<Box
-											display='flex'
-											sx={{
-												alignItems: 'center',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												height: '180px',
-												width: '180px',
-												borderRadius: '24px',
-												border: '2px solid'
-											}}>
-											<MetamaskLogo />
-											<Typography fontSize={14} fontWeight={500}>
-												MetaMask
-											</Typography>
-										</Box>
-									</Grid>
-									<Grid item xs={4}>
-										<Box
-											display='flex'
-											sx={{
-												alignItems: 'center',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												height: '180px',
-												width: '180px',
-												borderRadius: '24px',
-												border: '2px solid'
-											}}>
-											<WalletConnectLogo />
-											<Typography fontSize={14} fontWeight={500}>
-												Wallet Connect
-											</Typography>
-										</Box>
-									</Grid>
-									<Grid item xs={4}>
-										<Box
-											display='flex'
-											sx={{
-												alignItems: 'center',
-												flexDirection: 'column',
-												justifyContent: 'center',
-												height: '180px',
-												width: '180px',
-												borderRadius: '24px',
-												border: '2px solid'
-											}}>
-											<CoinbaseLogo />
-											<Typography fontSize={14} fontWeight={500}>
-												Coinbase Wallet
-											</Typography>
-										</Box>
-									</Grid>
-								</Grid>
-							</Box>
-						</Grid>
-					</Grid>
+					) : (
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'column',
+								justifyContent: 'center',
+								padding: '48px 32px 32px 32px',
+								alignItems: 'center'
+							}}>
+							<Typography fontWeight={800} fontSize={24} paddingBottom={16}>
+								🎉
+							</Typography>{' '}
+							<Typography fontWeight={800} fontSize={24} paddingBottom={16}>
+								{' '}
+								Congratulations, the room was created successfully!
+							</Typography>{' '}
+							<Typography fontWeight={400} fontSize={16} color='#00000066'>
+								Next, you can choose to complete your information and let others know you, or you can decorate your room
+								first
+							</Typography>
+							<Button sx={{ marginTop: '24px', marginBottom: '16px' }} variant='outlined'>
+								Start decorating the room
+							</Button>
+							<Typography onClick={() => {0}} fontWeight={500} fontSize={14} color='#00000066'>
+								Maybe later
+							</Typography>
+						</Box>
+					)}
 				</Box>
 			</Modal>
 		</Box>

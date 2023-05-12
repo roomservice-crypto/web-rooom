90abe4e8486121cc38a76ce0d87d62bb361dece7
iv-L33 <ivy@roomservice.gg>
2023-05-12 08:40:16 +0800

    test

--- src/hooks/useSignIn.ts
+++ src/hooks/useSignIn.ts
@@ -59,7 +59,7 @@ export function useSignInToken() {
 
   const token = useMemo(() => {
  
-    if(!account) return null
+    if(!account&&window) return null
     const storedToken = getCookie(API_TOKEN + account)
     if (storedToken) {
       axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${storedToken}`
